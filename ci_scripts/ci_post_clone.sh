bc695865f7f098a37a3297be84e5380d6dbcfe5d
Tiffany <tp284@cornell.edu>
2023-10-09 15:24:00 -0400

    Test build with release

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,10 +6,12 @@
 #  Created by Tiffany Pan on 9/27/23.
 #  
 
+echo "Installing Cocoapods..."
 brew install cocoapods
 pod deintegrate
 pod install
 
+echo "Installing Secrets..." 
 brew install wget
 wget -O ../Scoop/Supporting/GoogleService-Info.plist "$GOOGLE_SERVICE_PLIST"
 wget -O ../Scoop/Supporting/Keys.plist "$KEYS_PLIST"
