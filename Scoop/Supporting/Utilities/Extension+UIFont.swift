6f0a2c811d6c49568e0b35ce0abe6388e1eead9a
Caitlyn Jin <cj384@cornell.edu>
2023-10-18 09:44:40 -0400

    Added SF Pro custom fonts to target, adjusted extension file

--- Scoop/Supporting/Utilities/Extension+UIFont.swift
+++ Scoop/Supporting/Utilities/Extension+UIFont.swift
@@ -11,9 +11,9 @@ import UIKit
 extension UIFont {
     
     static let flowHeader = UIFont(name: "Sen-Regular", size: 24)
-    static let bodyBold = UIFont.systemFont(ofSize: 16, weight: .bold)
-    static let bodyNormal = UIFont.systemFont(ofSize: 16)
-    static let bodySemibold = UIFont.systemFont(ofSize: 16, weight: .semibold)
+    static let bodyBold = UIFont(name: "SFProDisplay-Bold", size: 16)
+    static let bodyNormal = UIFont(name: "SFProDisplay-Regular", size: 16)
+    static let bodySemibold = UIFont(name: "SFProDisplay-Semibold", size: 16)
     static let subheader = UIFont(name: "Rambla-Regular", size: 16)
     
 }
